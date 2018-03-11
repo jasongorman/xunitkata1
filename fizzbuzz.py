07038f8fa2c0c2518354d3165d1a092d86685d18
Will Price <will.price94@gmail.com>
2018-03-11 17:58:08 +0000

    Dynamically search for tests in the current file rather than having to manually add a call to each test in the main method, therefore no longer breaking open closed (main should be closed for modification, but open to extension)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,33 +1,41 @@
 import inspect
 
+import sys
+
 
 def fizzbuzz(n):
     if n % 3 == 0:
         return 'Fizz'
+    if n % 5 == 0:
+        return 'Buzz'
     return str(n)
-
-
-def test_fizzbuzz_6_is_Fizz():
-    assert_equals('Fizz', fizzbuzz(6))
-
-
-def test_fizzbuzz_3_is_Fizz():
-    assert_equals('Fizz', fizzbuzz(3))
-
-
-def test_fizzbuzz_2_is_2():
-    assert_equals('2', fizzbuzz(2))
 
 
 def test_fizzbuzz_1_is_1():
     assert_equals('1', fizzbuzz(1))
 
 
+def test_fizzbuzz_2_is_2():
+    assert_equals('2', fizzbuzz(2))
+
+
+def test_fizzbuzz_3_is_Fizz():
+    assert_equals('Fizz', fizzbuzz(3))
+
+
+def test_fizzbuzz_6_is_Fizz():
+    assert_equals('Fizz', fizzbuzz(6))
+
+
+def test_fizzbuzz_5_is_Buzz():
+    assert_equals('Buzz', fizzbuzz(5))
+
+
 def main():
-    test_fizzbuzz_1_is_1()
-    test_fizzbuzz_2_is_2()
-    test_fizzbuzz_3_is_Fizz()
-    test_fizzbuzz_6_is_Fizz()
+    tests = [fn for (name, fn) in inspect.getmembers(sys.modules[__name__])
+             if name.startswith('test') and inspect.isfunction(fn)]
+    for test in tests:
+        test()
 
 
 def assert_equals(expected_value, actual_value):
